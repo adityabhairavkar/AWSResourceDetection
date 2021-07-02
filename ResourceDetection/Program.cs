ec300c970792b6d52eb4762b454b5a5af6539ce0
Aditya Bhairavkar <74231226+adityabhairavkar@users.noreply.github.com>
2021-07-02 04:28:03 -0700

    Add files via upload

---
+++ ResourceDetection/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ResourceDetection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddDefaultAWSOptions(hostContext.Configuration.GetAWSOptions());
+            services.AddAWSService<IAmazonS3>();
+            services.AddHostedService<Worker>();
+        });
+    }
+}
---
+++ ResourceDetection/Worker.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+namespace ResourceDetection
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            string Hostname = Amazon.Util.EC2InstanceMetadata.Hostname;
+            string InstanceId = Amazon.Util.EC2InstanceMetadata.InstanceId;
+            string InstanceType = Amazon.Util.EC2InstanceMetadata.InstanceType;
+            string LocalHostname = Amazon.Util.EC2InstanceMetadata.LocalHostname;
+            string MacAddress = Amazon.Util.EC2InstanceMetadata.MacAddress;
+            string AvailabilityZone = Amazon.Util.EC2InstanceMetadata.AvailabilityZone;
+            string UserData = Amazon.Util.EC2InstanceMetadata.UserData;
+
+
+            _logger.LogInformation("Hostname: {name} , InstanceId :{InstanceId} , InstanceType :{InstanceType} ," +
+                " LocalHostname :{LocalHostname} , MacAddress : {MacAddress} ,AvailabilityZone : {AvailabilityZone}, UserData : {UserData}"
+                , Hostname , InstanceId , InstanceType , LocalHostname, MacAddress, AvailabilityZone , UserData);
+
+        }
+    }
+}
